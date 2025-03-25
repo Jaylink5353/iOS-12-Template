98c76d8b3b1bbac9c20e16c14145affa6be96372
Jaymes G <urmomrickroll1@gmail.com>
2025-03-24 20:04:21 -0400

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+Tested Working with Xcode 14, so you can develop apps for iOS 12.
+
+YOU MUST SET IT TO STORYBOARD MODE AS Swift UI CAME WITH iOS 13!!!
--- iOS12 Template/AppDelegate.swift
+++ iOS12 Template/AppDelegate.swift
@@ -1,36 +1,21 @@
-//
-//  AppDelegate.swift
-//  iOS12 Template
-//
-//  Created by Jaymes Goddard on 3/24/25.
-//
-
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootViewController = storyboard.instantiateInitialViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    // Add other necessary app lifecycle methods if needed
 }
+
 
